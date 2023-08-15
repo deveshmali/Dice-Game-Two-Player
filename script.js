d72b55d5603f95e1884d44d67cb4bbb9509b6af0
devesh <deveshmali567@gmail.com>
2023-08-15 13:06:06 +0530

    update in files

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ const btnHold = document.querySelector('.btn--hold');
 
 let scores, currentScore, activePlayer, playing;
 
-// Starting conditions here
+// Starting conditions here are
 const init = function () {
   scores = [0, 0];
   currentScore = 0;
